185a5a178637a2f7cf03fec823b5cad48e68fd47
TihkelK <116258789+TihkelK@users.noreply.github.com>
2024-11-28 13:56:14 +0200

    AAA

--- harjutused/yl10/js.js
+++ harjutused/yl10/js.js
@@ -5,17 +5,31 @@
 console.log("---")
 // Ostukorv
 const ostukorv = {
-tooted: [
-    { nimi:'Piim', hind:3.60, kogus:2 },
-    { nimi:'Leib', hind:2.00, kogus:1 },
-    { nimi:'Munad', hind:1.50, kogus:6 },
-    { nimi:'Juust', hind:4.20, kogus:1 },
-    { nimi:'Tomatid', hind:2.30, kogus:3 },
-]};
-function kuvasisu(t){
-    t.forEach((toode)) => {
-        // console.log("Nimi: " + toode.nimi + "Hind: " + toode.hind + "Kogus: " + toode.kogus)
-        console.log(toode)
+    tooted: [
+        { nimi:'Piim', hind:3.60, kogus:2 },
+        { nimi:'Leib', hind:2.00, kogus:1 },
+        { nimi:'Munad', hind:1.50, kogus:6 },
+        { nimi:'Juust', hind:4.20, kogus:1 },
+        { nimi:'Tomatid', hind:2.30, kogus:3 },
+    ],
+
+    kuva: function() {
+        this.tooted.forEach((toode) => {
+            console.log(`${toode.nimi} - ${toode.hind} EUR - Kogus: ${toode.kogus}`);
+        })
     }
-}
-kuvasisu(tooted);
+
+    lisa: function(nimi, hind, kogus) {
+        this.tooted.push({nimi, hind, kogus});
+        console.log(`Toode ${nimi} on lisatud ostukorvi.`);
+        
+    }
+    
+    summa: function() {
+        return this.tooted.reduce((this.summa, toode) => summa + (toode.hind * toode.kogus), 0).toFixed(2);
+    }
+};
+ostukorv.kuva();
+ostukorv.lisa('Salat', 3.70, 2);
+
+console.log('Ostukorvi kogu summa: ', ostukorv.summa());
